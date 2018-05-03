d150406d0e10da53ab571aa550029d41592cf350
Zhiyuan Ling <vito9509@gmail.com>
2018-05-02 20:20:46 -0400

    simplify the emission probability table construct code

--- src/main/java/edu/gwu/ai/codeknights/hmm/core/EmitProbTable.java
+++ src/main/java/edu/gwu/ai/codeknights/hmm/core/EmitProbTable.java
@@ -8,31 +8,43 @@ import java.util.Map;
  */
 public class EmitProbTable {
 
-  private final Map<Nucleotide, Map<Nucleotide, Map<Nucleotide, Double>>> table;
+  private final Map<Nucleotide, Map<Nucleotide, Map<Nucleotide, Integer>>> table;
 
   public EmitProbTable() {
     table = new LinkedHashMap<>();
   }
 
-  public void put(Nucleotide first, Nucleotide second, Nucleotide third, double prob) {
-    Map<Nucleotide, Map<Nucleotide, Double>> level1 = table.computeIfAbsent(first, k -> new LinkedHashMap<>());
-    Map<Nucleotide, Double> level2 = level1.computeIfAbsent(second, k -> new LinkedHashMap<>());
-    level2.put(third, prob);
+  /**
+   * only the value will be put in
+   *
+   * the double probability will be calculated
+   * */
+  public double put(Nucleotide first, Nucleotide second, Nucleotide third, int value) {
+    Map<Nucleotide, Map<Nucleotide, Integer>> level1 = table.computeIfAbsent(first, k -> new LinkedHashMap<>());
+    Map<Nucleotide, Integer> level2 = level1.computeIfAbsent(second, k -> new LinkedHashMap<>());
+    level2.put(third, value);
+    return getProb(first, second, third);
   }
 
-  public Map<Nucleotide, Map<Nucleotide, Double>> getLevel1(Nucleotide first){
+  public Map<Nucleotide, Map<Nucleotide, Integer>> getLevel1(Nucleotide first) {
     return table.get(first);
   }
 
-  public Map<Nucleotide, Double> getLevel2(Nucleotide first, Nucleotide second){
+  public Map<Nucleotide, Integer> getLevel2(Nucleotide first, Nucleotide second) {
     return getLevel1(first).get(second);
   }
 
-  public Double getProb(Nucleotide first, Nucleotide second, Nucleotide third){
+  public int getValue(Nucleotide first, Nucleotide second, Nucleotide third){
     return getLevel2(first, second).get(third);
   }
 
-  public Map<Nucleotide, Map<Nucleotide, Map<Nucleotide, Double>>> getLevel0() {
+  public Double getProb(Nucleotide first, Nucleotide second, Nucleotide third) {
+    Map<Nucleotide, Integer> level2 = getLevel2(first, second);
+    int sum = level2.values().stream().mapToInt(i -> i).sum();
+    return (double) level2.get(third) / (double) sum;
+  }
+
+  public Map<Nucleotide, Map<Nucleotide, Map<Nucleotide, Integer>>> getLevel0() {
     return table;
   }
 }
